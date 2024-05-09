3eee565527a11aaba4b5e1ab093e77384f77de61
ckk-12 <chaithrakk760@gmail.com>
2024-05-09 13:10:19 +0530

    first commit

---
+++ data.py
@@ -0,0 +1,105 @@
+question_data = [
+    {
+        "category": "science: computers",
+        "type": "boolean",
+        "difficuity": "medium",
+        "question": "Does Python allow function overloading.",
+        "correct_answer": "False",
+        "incorrect_answer": [
+            "True"
+        ]
+    },
+    
+    {
+        "category": "science: computers",
+        "type": "boolean",
+        "difficuity": "medium",
+        "question": "Are arrays in Python resizable.",
+        "correct_answer": "True",
+        "incorrect_answer": [
+            "False"
+        ]
+    },
+    {
+        "category": "science: computers",
+        "type": "boolean",
+        "difficuity": "medium",
+        "question": "Is CSS a programming language.",
+        "correct_answer": "False",
+        "incorrect_answer": [
+            "True"
+        ]
+    },
+    {
+        "category": "science: computers",
+        "type": "boolean",
+        "difficuity": "medium",
+        "question": "Does the break statement in Python only exit the innermost loop.",
+        "correct_answer": "True",
+        "incorrect_answer": [
+            "False"
+        ]
+    },
+    {
+        "category": "science: computers",
+        "type": "boolean",
+        "difficuity": "medium",
+        "question": "Does HTML provide a way to create interactive web pages.",
+        "correct_answer": "False",
+        "incorrect_answer": [
+            "True"
+        ]
+    },
+    {
+        "category": "science: computers",
+        "type": "boolean",
+        "difficuity": "medium",
+        "question": " In Python, is it possible to use a list comprehension to generate a list of prime numbers.",
+        "correct_answer": "True",
+        "incorrect_answer": [
+            "False"
+        ]
+    },
+    {
+        "category": "science: computers",
+        "type": "boolean",
+        "difficuity": "medium",
+        "question": " In Java, can a method be both abstract and final.",
+        "correct_answer": "False",
+        "incorrect_answer": [
+            "True"
+        ]
+    },
+    {
+        "category": "science: computers",
+        "type": "boolean",
+        "difficuity": "medium",
+        "question": "Can a Python function have a variable number of arguments.",
+        "correct_answer": "True",
+        "incorrect_answer": [
+            "False"
+        ]
+    },
+    {
+        "category": "science: computers",
+        "type": "boolean",
+        "difficuity": "medium",
+        "question": "Can a function return multiple values in Python.",
+        "correct_answer": "True",
+        "incorrect_answer": [
+            "False"
+        ]
+    },
+    {
+        "category": "science: computers",
+        "type": "boolean",
+        "difficuity": "medium",
+        "question": "Are dictionaries ordered collections in Python.",
+        "correct_answer": "False",
+        "incorrect_answer": [
+            "True"
+        ]
+    },
+
+
+]
